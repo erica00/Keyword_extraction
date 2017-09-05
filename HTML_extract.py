c05bd0c06a0677f4ab7d312bc7c900e2ef929167
erica00 <31665998+erica00@users.noreply.github.com>
2017-09-06 02:13:03 +0800

    Update HTML_extract.py

--- HTML_extract.py
+++ HTML_extract.py
@@ -8,19 +8,20 @@ Created on Tue Sep 05 23:02:08 2017
 #导入bs4库
 from bs4 import BeautifulSoup
 import urllib2
+'''
 #test html
-#html = """
-#<html><head><title>The Dormouse's story</title></head>
-#<body>
-#<p class="title" name="dromouse"><b>The Dormouse's story</b></p>
-#<p class="story">Once upon a time there were three little sisters; and their names were
-#<a href="http://example.com/elsie" class="sister" id="link1"><!-- Elsie --></a>,
-#<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-#<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-#and they lived at the bottom of a well.</p>
-#<p class="story">...</p>
-#"""
-
+html = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title" name="dromouse"><b>The Dormouse's story</b></p>
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1"><!-- Elsie --></a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+<p class="story">...</p>
+"""
+'''
 
 request = urllib2.Request("https://news.ycombinator.com/newest")
 response = urllib2.urlopen(request,timeout = 40)
